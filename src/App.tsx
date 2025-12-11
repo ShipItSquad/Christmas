a59b50bcfa3693576f9f33d300a6adfb617be167
parsa-mir <parsa.miraghaie@gmail.com>
2025-12-11 15:24:16 +0200

    dist update

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { APITester } from "./APITester";
 import { ChristmasScene } from "./components/ChristmasScene";
 import "./index.css";
+import { motion } from "motion/react";
 export function App() {
   return (
-    <>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 , backgroundColor: "black"}} transition={{ duration: 2 , ease: "easeInOut" }}>
       <ChristmasScene />
       <div className="pointer-events-none fixed inset-0 flex items-center justify-center z-50 bg-blue-600/20 text-white">
- <div className="font-sans font-bold flex flex-col items-center justify-center  p-4 rounded-lg">
+ <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 , y: 10  }} transition={{ duration: 1 , delay: 3 , ease: "easeInOut" }} className="font-sans font-bold flex flex-col items-center justify-center  p-4 rounded-lg">
   <h1 className="text-4xl"> AdventureClub</h1>
-  <h1 className="text-2xl font-normal">Merry Christmas</h1>
+  <h1 className="text-xl font-normal">Merry Christmas</h1>
 
- </div>
+ </motion.div>
 </div> 
-    </>
+    </motion.div>
   );
 }
 
